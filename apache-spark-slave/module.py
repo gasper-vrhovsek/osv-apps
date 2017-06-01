c8988bd82ccf1a7a1bb9c1c21ba28eaedb05867f
Gasper Vrhovsek <gasper.vrhovsek@gmail.com>
2017-06-01 06:36:16 +0200

    Modified run command for slave to suit apache spark 2.1.1

--- apache-spark-slave/module.py
+++ apache-spark-slave/module.py
@@ -2,4 +2,6 @@ from osv.modules import api
 
 api.require('java')
 
-default = api.run("--cwd=/spark /java.so -Xms512m -Xmx512m -cp /spark/conf:/spark/lib/spark-assembly-1.4.0-hadoop2.6.0.jar:/spark/lib/datanucleus-core-3.2.10.jar:/spark/lib/datanucleus-rdbms-3.2.9.jar:/spark/lib/datanucleus-api-jdo-3.2.6.jar -Dscala.usejavacp=true org.apache.spark.deploy.SparkSubmit --class org.apache.spark.repl.Main spark-shell")
+default = api.run("--cwd=/spark /java.so -Xms512m -Xmx512m -cp /spark/conf:/spark/jars/* -Dscala.usejavacp=true org.apache.spark.deploy.worker.Worker 10.10.100.9:7077")
+
+
