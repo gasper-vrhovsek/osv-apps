0bdb2e08b11f1837cd4e1fe7485500bbd497dacb
Gasper Vrhovsek <gasper.vrhovsek@gmail.com>
2017-06-07 12:44:34 +0200

    Modified configuration of apache-spark-slave app made to run locally built version of apache spark containing OsvProcessBuilder.

--- apache-spark-slave/module.py
+++ apache-spark-slave/module.py
@@ -2,6 +2,6 @@ from osv.modules import api
 
 api.require('java')
 
-default = api.run("--cwd=/spark /java.so -Xms512m -Xmx512m -cp /spark/conf:/spark/jars/* -Dscala.usejavacp=true org.apache.spark.deploy.worker.Worker 10.10.100.9:7077")
+default = api.run("--cwd=/spark /java.so -Xms512m -Xmx512m -cp /spark/conf:/spark/jars/* -Dscala.usejavacp=true org.apache.spark.deploy.worker.Worker 172.16.122.4:7077")
 
 
