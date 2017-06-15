56b71caa9af8bf405898022186a7d1ae9cca7fdb
Gasper Vrhovsek <gasper.vrhovsek@gmail.com>
2017-06-15 07:12:54 +0200

    Update usr.manifest makefile and module.py to latest dev version.

--- apache-spark-slave/module.py
+++ apache-spark-slave/module.py
@@ -2,6 +2,7 @@ from osv.modules import api
 
 api.require('java')
 
-default = api.run("--cwd=/spark /java.so -Xms512m -Xmx512m -cp /spark/conf:/spark/jars/* -Dscala.usejavacp=true org.apache.spark.deploy.worker.Worker 172.16.122.4:7077")
+#default = api.run("--cwd=/spark /java.so -Xms512m -Xmx512m -cp /spark/conf:/spark/jars/* -Dscala.usejavacp=true org.apache.spark.deploy.worker.Worker 172.16.122.4:7077")
+default = api.run("--cwd=/spark /java.so -Xms512m -Xmx512m io.osv.isolated.MultiJarLoader -mains /etc/javamains")
 
 
